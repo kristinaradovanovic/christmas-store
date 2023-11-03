80297f89a938a6f3f572dc4a226b5eb5d1318781
Kristina Radovanovic <cristinaradovanovic@gmail.com>
2023-11-03 14:12:23 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <header>
+      The Holiday Home Shop
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+      @all rigts reserved
+    </footer>
+  </div>
+  )
+}
+
+export default App
